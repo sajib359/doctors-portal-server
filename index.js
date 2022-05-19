97863ee9c30f0b0af8089c628f22ad1247839d2d
Sajib Hossain <sajib.sj359@gmail.com>
2022-05-19 12:25:26 +0600

    create mongodb server

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ async function run(){
     try{
         await client.connect();
         console.log('Database Connected');
+        const serviceCollection = client.db('doctorPortal').collection('services');
+        
+        app.get('/service', async(req,res)=>{
+          const query ={};
+          const cursor = serviceCollection.find(query);
+          const services = await cursor.toArray();
+          res.send(services);
+        })
     }
     finally{
 
